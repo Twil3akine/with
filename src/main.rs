0cb32dc300c8af42d4a8e9f9b272c36e6addfc5f
twil3akine <twil3akine@gmail.com>
2025-11-19 18:35:17 +0900

    引数の受け取り、引数のチェックを実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::{env, eprintln, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect::<Vec<String>>();
+
+    if args.len() <= 1 {
+        eprintln!("Usage: with <COMMAND>");
+        process::exit(1);
+    }
+
+    let cmd: &String = &args[1];
 }
